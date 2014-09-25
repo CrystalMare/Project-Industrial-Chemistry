16aeeeb78d04449519b03fb834a348a9d5844ec1
CrystalMare <admin@heaven-craft.net>
2014-09-26 01:48:09 +0200

    Fixed Block

--- src/main/java/net/heavencraft/industrialchemistry/init/PICBlocks.java
+++ src/main/java/net/heavencraft/industrialchemistry/init/PICBlocks.java
@@ -1,13 +1,18 @@
 package net.heavencraft.industrialchemistry.init;
 
+import net.heavencraft.industrialchemistry.block.BlockPIC;
+import net.heavencraft.industrialchemistry.block.BlockTesting;
+import net.heavencraft.industrialchemistry.reference.Names;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class PICBlocks
 {
 	//Declare a public static final BlockPIC here
 	
+	public static final BlockPIC blockTesting = new BlockTesting();
+	
 	public static void init()
 	{
-		//Register the BlockPIC here
+		GameRegistry.registerBlock(blockTesting, Names.Block.BLOCKTESTING);
 	}
 }
