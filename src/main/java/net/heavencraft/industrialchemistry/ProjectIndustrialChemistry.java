e54e6a4804f84fe792afcaa7a8fc05c19a1e8395
ModdingChase <pd.van.lill@gmail.com>
2014-09-25 23:52:19 +0200

    Setting up modfile

---
+++ src/main/java/net/heavencraft/industrialchemistry/ProjectIndustrialChemistry.java
@@ -0,0 +1,9 @@
+package net.heavencraft.industrialchemistry;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "pic", name = "Project Industrial Chemistry", version = "0.1 beta")
+public class ProjectIndustrialChemistry
+{
+	
+}
