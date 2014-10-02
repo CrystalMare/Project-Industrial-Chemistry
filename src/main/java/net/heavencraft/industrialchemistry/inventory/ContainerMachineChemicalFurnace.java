1251703f37e5a5dcc84f71a29afceecb98e8d004
ModdingChase <pd.van.lill@gmail.com>
2014-10-02 04:45:30 +0200

    Potentially added all in gui item handling in Furnace! Shift Clicking doesnt crash now

--- src/main/java/net/heavencraft/industrialchemistry/inventory/ContainerMachineChemicalFurnace.java
+++ src/main/java/net/heavencraft/industrialchemistry/inventory/ContainerMachineChemicalFurnace.java
@@ -1,9 +1,12 @@
 package net.heavencraft.industrialchemistry.inventory;
 
+import net.heavencraft.industrialchemistry.handlers.NewRecipeHandler;
+import net.heavencraft.industrialchemistry.handlers.Recipe;
 import net.heavencraft.industrialchemistry.network.PacketHandler;
 import net.heavencraft.industrialchemistry.network.packet.PacketChemicalFurnace;
 import net.heavencraft.industrialchemistry.reference.Textures;
 import net.heavencraft.industrialchemistry.tileentity.TEMachineChemicalFurnace;
+import net.heavencraft.industrialchemistry.util.CollectionUtils;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -27,16 +30,96 @@ public class ContainerMachineChemicalFurnace extends BaseContainer
 	public ContainerMachineChemicalFurnace(InventoryPlayer player, TEMachineChemicalFurnace tile)
 	{
 		this.machineChemicalFurnaceTE = tile;
-		this.drawInv(player, 0, 78);
 		addSlot(tile, 0, inputSlot.getX(), inputSlot.getY());
 		addSlot(tile, 1, outputSlot.getX(), outputSlot.getY());
+		this.drawInv(player, 0, 78);
 	}
 	
 	@Override
 	public ItemStack transferStackInSlot(EntityPlayer player, int slotIndex)
 	{
-		ItemStack inputSlot = machineChemicalFurnaceTE.getStackInSlot(0);
+		ItemStack stackToTrans;
+		stackToTrans = getSlot(slotIndex).getStack();
+		ItemStack inputSlot = getSlot(0).getStack();
+		Recipe recipe = NewRecipeHandler.getRecipe(TEMachineChemicalFurnace.class, CollectionUtils.getList(new Object[] { stackToTrans }));
+		if (slotIndex == 1)
+		{
+			for (int i = 0; i < 35; i++)
+			{
+				stackToTrans = getSlot(slotIndex).getStack();
+				inputSlot = getSlot(0).getStack();
+				if (player.inventory.getStackInSlot(i) != null && player.inventory.getStackInSlot(i).isItemEqual(stackToTrans))
+				{
+					int maxsize = stackToTrans.getMaxStackSize();
+					int stacksizeone = player.inventory.getStackInSlot(i).stackSize;
+					int stacksizetwo = stackToTrans.stackSize;
+					if (stackToTrans.getMaxStackSize() < player.inventory.getStackInSlot(i).stackSize + stackToTrans.stackSize)
+					{
+						int stackSizeBefore = player.inventory.getStackInSlot(i).stackSize;
+						player.inventory.setInventorySlotContents(i, new ItemStack(player.inventory.getStackInSlot(i).getItem(), player.inventory.getStackInSlot(i).getMaxStackSize()));
+						getSlot(1).putStack(new ItemStack(player.inventory.getStackInSlot(i).getItem(), stackSizeBefore));
+					}
+					else
+					{
+						player.inventory
+						        .setInventorySlotContents(i, new ItemStack(player.inventory.getStackInSlot(i).getItem(), player.inventory.getStackInSlot(i).stackSize + stackToTrans.stackSize));
+						getSlot(1).putStack(null);
+						return null;
+					}
+				}
+			}
+			for (int i = 0; i < player.inventory.getSizeInventory(); i++)
+			{
+				if (player.inventory.getStackInSlot(i) == null)
+				{
+					player.inventory.setInventorySlotContents(i, stackToTrans);
+					getSlot(1).putStack(null);
+					return null;
+				}
+			}
+			
+			return null;
+		}
+		
+		if (recipe != null && inputSlot != null)
+		{
+			if (inputSlot.isItemEqual(stackToTrans))
+			{
+				if (inputSlot.getMaxStackSize() < inputSlot.stackSize + stackToTrans.stackSize)
+				{
+					int stackSizeBefore = inputSlot.stackSize;
+					getSlot(0).putStack(new ItemStack(inputSlot.getItem(), inputSlot.getMaxStackSize()));
+					getSlot(slotIndex).putStack(new ItemStack(inputSlot.getItem(), stackSizeBefore));
+					return null;
+				}
+				else
+				{
+					getSlot(0).putStack(new ItemStack(inputSlot.getItem(), inputSlot.stackSize + stackToTrans.stackSize));
+					getSlot(slotIndex).putStack(null);
+					
+				}
+			}
+			else
+			{
+				return null;
+			}
+		}
+		else if (recipe != null && inputSlot == null)
+		{
+			getSlot(0).putStack(stackToTrans.copy());
+			getSlot(slotIndex).putStack(null);
+		}
 		return null;
+	}
+	
+	@Override
+	public ItemStack slotClick(int slotID, int p_75144_2_, int mode, EntityPlayer player)
+	{
+		if(player.inventory.getItemStack() != null && slotID == 1)
+		{
+			return null;
+		}
+	    return super.slotClick(slotID, p_75144_2_, mode, player);
 	}
 	
 	@Override
