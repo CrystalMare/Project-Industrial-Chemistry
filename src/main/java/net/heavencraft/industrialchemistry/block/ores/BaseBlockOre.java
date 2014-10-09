b77736880a663fa61411ae55741ba6fae9c41530
CrystalMare <admin@heaven-craft.net>
2014-10-10 00:16:16 +0200

    Removed BaseBlockOre

--- src/main/java/net/heavencraft/industrialchemistry/block/ores/BaseBlockOre.java
+++
@@ -1,5 +0,0 @@
-package net.heavencraft.industrialchemistry.block.ores;
-
-import net.heavencraft.industrialchemistry.block.BaseBlock;
-
-public abstract class BaseBlockOre extends BaseBlock { }
--- src/main/java/net/heavencraft/industrialchemistry/block/ores/BlockOreMalachite.java
+++ src/main/java/net/heavencraft/industrialchemistry/block/ores/BlockOreMalachite.java
@@ -1,10 +1,11 @@
 package net.heavencraft.industrialchemistry.block.ores;
 
+import net.heavencraft.industrialchemistry.block.BaseBlock;
 import net.heavencraft.industrialchemistry.creativetab.CreativeTab;
 import net.heavencraft.industrialchemistry.reference.Names;
 import net.heavencraft.industrialchemistry.reference.Textures;
 
-public class BlockOreMalachite extends BaseBlockOre
+public class BlockOreMalachite extends BaseBlock
 {
 	public BlockOreMalachite()
 	{
